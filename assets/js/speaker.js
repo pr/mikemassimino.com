d522550f5fcd3e36908f45db87f2e91da8902e7c
Peter Richards <peter@peter.net>
2019-01-13 11:28:36 -0600

    1.0 speaking page

---
+++ assets/js/speaker.js
@@ -0,0 +1,10 @@
+$('.speaker-videos').slick({
+	accessibility: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	adaptiveHeight: true,
+	infinite: true,
+	fade: true,
+	cssEase: 'linear',
+});
