d8c999b9802ea3487134ce97f79db25671359b6f
Peter Richards <peter@peter.net>
2023-10-08 15:45:26 -0400

    Moonshot

---
+++ assets/js/moonshot.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+	$('.moonshot-reviews').slick({
+
+		centerMode: false,
+		centerPadding: '30px',
+		slidesToShow: 3,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight){
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
